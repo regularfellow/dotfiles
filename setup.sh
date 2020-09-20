a565726202d73126ff038557e66aa8abdc4a9d39
regularfellow <59539975+regularfellow@users.noreply.github.com>
2020-09-20 17:39:55 +0300

    Install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ if [ `whoami` == root ]; then
     echo Run this script as user.
     exit
 fi
+
+# Set shell to zsh
+echo "Installing oh-my-zsh..."
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
