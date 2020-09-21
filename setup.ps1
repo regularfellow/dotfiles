fd6c1cf758d10ad4c65811e02a5a7b8dbe95f84b
regularfellow <59539975+regularfellow@users.noreply.github.com>
2020-09-21 17:05:33 +0300

    Setup windows terminal config

--- setup.ps1
+++ setup.ps1
@@ -13,3 +13,13 @@ Write-Output "Setting up..."
 # cmd /c scoop install git ffmpeg aria2 git-crypt
 # cmd /c scoop bucket add nerd-fonts
 # cmd /c scoop install FiraCode
+
+# Windows Terminal
+Write-Output "Installing Windows Terminal config..."
+$TerminalSettingsDest = "$Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+if (Test-Path -Path $TerminalSettingsDest) {
+    (Get-Item $TerminalSettingsDest).Delete()
+}
+$null = New-Item -Path $TerminalSettingsDest -ItemType SymbolicLink -Value ".\Microsoft.WindowsTerminal_8wekyb3d8bbwe\settings.json"
+$null = New-Item -Path "C:\Users\Public\Pictures\Microsoft.WindowsTerminal_8wekyb3d8bbwe" -ItemType Directory -Force
+$null = Copy-Item ".\Microsoft.WindowsTerminal_8wekyb3d8bbwe\ssh.png" -Destination "C:\Users\Public\Pictures\Microsoft.WindowsTerminal_8wekyb3d8bbwe\ssh.png"
